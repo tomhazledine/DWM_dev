36f18daa72cb9606140ea3caa0c02916500b5b61
DWM <tom@digitalwebmedia.co.uk>
2014-11-07 18:53:34 +0000

    fixed local modernizr path in head

--- gulpfile.js
+++ gulpfile.js
@@ -148,7 +148,7 @@ gulp.task('watch', function() {
     gulp.watch('uncompressed/images/**', ['images', 'jekyll']);
     gulp.watch('uncompressed/fonts/**', ['fonts', 'jekyll']);
     gulp.watch('uncompressed/icons/**', ['iconfont', 'jekyll']);
-    gulp.watch('*.html',['jekyll']);
+    gulp.watch(['*.html','_includes/**'],['jekyll']);
 
     gulp.watch(['_site/**']).on('change', function(file) {
         livereload(server).changed(file.path);
