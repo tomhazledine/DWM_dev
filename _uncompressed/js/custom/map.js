a3efc9970e1307c139b45a368ba036d60a6abd95
Thomas Hazledine <tom@digitalwebmedia.co.uk>
2015-06-15 08:57:44 +0100

    homepage quote block styling

--- _uncompressed/js/custom/map.js
+++ _uncompressed/js/custom/map.js
@@ -10,102 +10,103 @@ var mapOffsetXRaw = parseFloat($('.propMapLatOffset').text());
 var mapOffsetYRaw = parseFloat($('.propMapLngOffset').text());
 var customMarkerIcon = '';//'/wp-content/themes/penvale/assets/images/paw.png';
 
+if (mapBox.length) {
 
-var mapStyle = [
-  {
-    "featureType":"landscape",
-    "stylers":[
-      {"saturation":-100},
-      {"lightness":65},
-      {"visibility":"on"}
-    ]},
+  var mapStyle = [
     {
-      "featureType":"poi",
+      "featureType":"landscape",
       "stylers":[
         {"saturation":-100},
-        {"lightness":51},
-        {"visibility":"simplified"}
-      ]
-    },
-    {
-      "featureType":"road.highway",
-      "stylers":[
-        {"saturation":-100},
-        {"visibility":"simplified"}
-      ]
-    },
-    {
-      "featureType":"road.arterial",
-      "stylers":[
-        {"saturation":-100},
-        {"lightness":30},
+        {"lightness":65},
         {"visibility":"on"}
-      ]
-    },
-    {
-      "featureType":"road.local",
-      "stylers":[
-        {"saturation":-100},
-        {"lightness":40},
-        {"visibility":"on"}
-      ]
-    },
-    {
-      "featureType":"transit",
-      "stylers":[
-        {"saturation":-100},
-        {"visibility":"simplified"}
-      ]
-    },
-    {
-      "featureType":"administrative.province",
-      "stylers":[
-        {"visibility":"off"}
-      ]
-    },
-    {
-      "featureType":"water",
-      "elementType":"labels",
-      "stylers":[
-        {"visibility":"on"},
-        {"lightness":-25},
-        {"saturation":-100}
-      ]
-    },
-    {
-      "featureType":"water",
-      "elementType":"geometry",
-      "stylers":[
-        {"hue":"#ffff00"},
-        {"lightness":-25},
-        {"saturation":-97}
-      ]
-    },
-    {
-      "featureType":"poi",
-      "stylers":[
-        {"visibility":"off"}
-      ]
-    }
-  ];
+      ]},
+      {
+        "featureType":"poi",
+        "stylers":[
+          {"saturation":-100},
+          {"lightness":51},
+          {"visibility":"simplified"}
+        ]
+      },
+      {
+        "featureType":"road.highway",
+        "stylers":[
+          {"saturation":-100},
+          {"visibility":"simplified"}
+        ]
+      },
+      {
+        "featureType":"road.arterial",
+        "stylers":[
+          {"saturation":-100},
+          {"lightness":30},
+          {"visibility":"on"}
+        ]
+      },
+      {
+        "featureType":"road.local",
+        "stylers":[
+          {"saturation":-100},
+          {"lightness":40},
+          {"visibility":"on"}
+        ]
+      },
+      {
+        "featureType":"transit",
+        "stylers":[
+          {"saturation":-100},
+          {"visibility":"simplified"}
+        ]
+      },
+      {
+        "featureType":"administrative.province",
+        "stylers":[
+          {"visibility":"off"}
+        ]
+      },
+      {
+        "featureType":"water",
+        "elementType":"labels",
+        "stylers":[
+          {"visibility":"on"},
+          {"lightness":-25},
+          {"saturation":-100}
+        ]
+      },
+      {
+        "featureType":"water",
+        "elementType":"geometry",
+        "stylers":[
+          {"hue":"#ffff00"},
+          {"lightness":-25},
+          {"saturation":-97}
+        ]
+      },
+      {
+        "featureType":"poi",
+        "stylers":[
+          {"visibility":"off"}
+        ]
+      }
+    ];
 
 
 
-if(!isNaN(mapOffsetXRaw)){
-  var mapOffsetX = mapOffsetXRaw;
-  //console.log('mapOffsetX = '+mapOffsetX);
-}else{
-  var mapOffsetX = 0;
-  //console.log('mapOffsetX = null');
-}
-if(!isNaN(mapOffsetYRaw)){
-  var mapOffsetY = mapOffsetYRaw;
-}else{
-  var mapOffsetY = 0;
-}
+  if(!isNaN(mapOffsetXRaw)){
+    var mapOffsetX = mapOffsetXRaw;
+    //console.log('mapOffsetX = '+mapOffsetX);
+  }else{
+    var mapOffsetX = 0;
+    //console.log('mapOffsetX = null');
+  }
+  if(!isNaN(mapOffsetYRaw)){
+    var mapOffsetY = mapOffsetYRaw;
+  }else{
+    var mapOffsetY = 0;
+  }
 
 
-if(mapBox.length){
+// if(mapBox.length){
   //console.log('There is a map on this page.');
 
   var map;// Setup the Google Maps variable
